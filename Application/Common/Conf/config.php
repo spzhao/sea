3c9850cdbfc97cb74ca89dc07d7d1a3087c29082
spzhao <ediszhao@sina.com>
2016-02-03 15:46:16 +0800

    change config file

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -19,7 +19,7 @@ return array(
     //'MODULE_ALLOW_LIST'  => array('Home','Admin'),
 
     /* 系统数据加密设置 */
-    'DATA_AUTH_KEY' => 'LmCx:2E+z[!kD"1N>U~3cB{f9WSqZt]Tl/V&PM|0', //默认数据加密KEY
+    'DATA_AUTH_KEY' => 'r^~@FA`=9-fEwD3KH7;i,md(P+[s*ILYjku6qGQ$', //默认数据加密KEY
 
     /* 调试配置 */
     'SHOW_PAGE_TRACE' => true,
@@ -39,13 +39,31 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'onethink', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '',  // 密码
-    'DB_PORT'   => '3306', // 端口
+    'DB_HOST'   => 'w.rdc.sae.sina.com.cn', // 服务器地址
+    'DB_NAME'   => 'app_spzhaol', // 数据库名
+    'DB_USER'   => 'y20n1ylo5m', // 用户名
+    'DB_PWD'    => 'zl2li51z405ym5m4my3k0xyw0yx5zky4mhj54wzm',  // 密码
+    'DB_PORT'   => '3307', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
     /* 文档模型配置 (文档模型核心配置，请勿更改) */
     'DOCUMENT_MODEL_TYPE' => array(2 => '主题', 1 => '目录', 3 => '段落'),
 );
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+/**
+ * UCenter客户端配置文件
+ * 注意：该配置文件请使用常量方式定义
+ */
+
+define('UC_APP_ID', 1); //应用ID
+define('UC_API_TYPE', 'Model'); //可选值 Model / Service
+define('UC_AUTH_KEY', 'r^~@FA`=9-fEwD3KH7;i,md(P+[s*ILYjku6qGQ$'); //加密KEY
+define('UC_DB_DSN', 'mysqli://y20n1ylo5m:zl2li51z405ym5m4my3k0xyw0yx5zky4mhj54wzm@w.rdc.sae.sina.com.cn:3307/app_spzhaol'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_TABLE_PREFIX', 'onethink_'); // 数据表前缀，使用Model方式调用API必须配置此项
